0f5f088d7507fbadd5a83ff37e09929f8a29fd75
Fabian Lindgren <lindgrenfabian@gmail.com>
2023-09-07 19:33:29 +0200

    Add controller vibration for wall detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -27,6 +27,8 @@ namespace Spooky
         private InteractableController _interactableController;
         public SpookyInput Input { get; private set; }
 
+        [SerializeField] private float _maxMotorSpeed = 0.5f;
+
         private void Start()
         {
             _rigidbody = GetComponent<Rigidbody>();
@@ -56,11 +58,14 @@ namespace Spooky
             Vector2 rotationInputVector = Input.Player.Look.ReadValue<Vector2>();
             float rotationInput = rotationInputVector.x;
 
-            _transform.position += _transform.rotation * new Vector3(0, 0, movementInput) * (MovementSpeed * Time.fixedDeltaTime);
+            // Target movement is the movement we *want* to make this frame
+            Vector3 targetMovement = _transform.rotation * new Vector3(0, 0, movementInput) * (MovementSpeed * Time.fixedDeltaTime);
+            _transform.position += targetMovement;
             _transform.rotation *= Quaternion.Euler(0, rotationInput * RotationSpeed * Time.fixedDeltaTime, 0);
 
-            Vector3 movedDistance = transform.position - _lastPosition;
-            _unitsSinceLastStep += Mathf.Abs(movedDistance.magnitude);
+            // Actual movement is the movement we *actually* made this frame
+            Vector3 actualMovement = transform.position - _lastPosition;
+            _unitsSinceLastStep += Mathf.Abs(actualMovement.magnitude);
 
             // Check if we should play a footstep
             if (_unitsSinceLastStep > _unitsBetweenStep)
@@ -74,12 +79,24 @@ namespace Spooky
                 Destroy(a, clip.length);
             }
 
+            float movementPercentage = actualMovement.magnitude / targetMovement.magnitude;
+            float rotationSign = Mathf.Sign(rotationInput);
 
-            // set gamepad motor speeds based on if we ran into a wall
-            if (Gamepad.current != null)
+            // Set gamepad motor speeds based on reduced movement percentage
+            if (Gamepad.current != null && movementPercentage < 0.98f)
             {
-            }
+                // Split the motorSpeed based on rotation direction
+                float leftMotorSpeed = 1 - movementPercentage;
+                float rightMotorSpeed = 1 - movementPercentage;
 
+                // Adjust motorSpeeds based on rotation direction
+                if (rotationSign > 0)
+                    rightMotorSpeed *= 1.5f;
+                else if (rotationSign < 0)
+                    leftMotorSpeed *= 1.5f;
+
+                Gamepad.current.SetMotorSpeeds(leftMotorSpeed * _maxMotorSpeed, rightMotorSpeed * _maxMotorSpeed);
+            }
 
             // Cache the current position over to the next frame
             _lastPosition = transform.position;
