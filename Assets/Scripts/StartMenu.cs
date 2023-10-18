e335dccca3f033c33a81a353440853455a46ed74
Fab <lindgrenfabian@gmail.com>
2023-10-18 11:47:36 +0200

    add main menu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Spooky
+{
+    public class StartMenu : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.anyKeyDown)
+                SceneManager.LoadScene("StartScene");
+        }
+    }
+}
